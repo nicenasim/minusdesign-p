3fbe2522794d21a433d6b142863ce64424baaa39
nicenasim <nicenasim@gmail.com>
2024-08-20 12:51:40 +0600

    first commit

---
+++ assets/use-timeout.3b70c684.js
@@ -0,0 +1,1 @@
+import{v as u}from"./QSpinner.ff70a72e.js";import{y as i,d as m,g as s}from"./index.d3e2243d.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
