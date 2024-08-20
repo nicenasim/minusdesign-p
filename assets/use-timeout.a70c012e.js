24ed72e5e976cef642c1355948d6343f1ce1e1b5
nicenasim <nicenasim@gmail.com>
2024-08-20 15:24:33 +0600

    verson 2

---
+++ assets/use-timeout.a70c012e.js
@@ -0,0 +1,1 @@
+import{v as u}from"./QSpinner.464cd8b7.js";import{y as i,d as m,g as s}from"./index.4a54b43f.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
