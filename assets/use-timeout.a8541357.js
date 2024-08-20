f521b914e8232edbc622062274cf498aea5659b7
nicenasim <nicenasim@gmail.com>
2024-08-20 17:00:11 +0600

    verson 4

---
+++ assets/use-timeout.a8541357.js
@@ -0,0 +1,1 @@
+import{v as u}from"./QSpinner.6b74fcce.js";import{y as i,d as m,g as s}from"./index.af447ece.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
