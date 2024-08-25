36996b377283b969cae41818caaf905899efd2bc
nicenasim <nicenasim@gmail.com>
2024-08-25 17:14:49 +0600

    verson 8

---
+++ assets/use-timeout.60191ed5.js
@@ -0,0 +1,1 @@
+import{v as u}from"./QBtn.06d382dc.js";import{y as i,d as m,g as s}from"./index.25289c93.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
