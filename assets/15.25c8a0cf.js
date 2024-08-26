02d886aba2d56830c12834c8c7cdd62c5a9d1dfa
nicenasim <nicenasim@gmail.com>
2024-08-26 18:50:49 +0600

    verson 9

---
+++ assets/15.25c8a0cf.js
@@ -0,0 +1,1 @@
+var a="/assets/15.2c991bd6.jpg";export{a as i};
---
+++ assets/use-timeout.aa5578d3.js
@@ -0,0 +1,1 @@
+import{v as u}from"./QBtn.5ac9d7f3.js";import{y as i,d as m,g as s}from"./index.13770dd1.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
